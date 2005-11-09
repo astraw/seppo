fad4735d6f50181b6fb81842defc049113bc13a9
astraw <astraw@d8880f02-dcdd-0310-a0d1-a85aa60ae51e>
2005-11-09 20:08:15 +0000

    preserve file attributes on upload
    git-svn-id: https://code.astraw.com/astraw-private-repo@248 d8880f02-dcdd-0310-a0d1-a85aa60ae51e

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 rest2html -dgs seppo.txt seppo.html
 rest2html -dgs seppo-changes.txt seppo-changes.html
-scp -r seppo.txt seppo.html seppo-changes.txt seppo-changes.html seppo-download its.caltech.edu:public_html/
+scp -p -r seppo.txt seppo.html seppo-changes.txt seppo-changes.html seppo-download its.caltech.edu:public_html/
