5ac890980c87f108512809d110b76d079fd5a4a0
astraw <astraw@d8880f02-dcdd-0310-a0d1-a85aa60ae51e>
2006-01-23 06:43:43 +0000

    made setuptools based package
    git-svn-id: https://code.astraw.com/astraw-private-repo@301 d8880f02-dcdd-0310-a0d1-a85aa60ae51e

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="seppo",
-    version="20051109",
+    version="20051211",
     license="BSD",
     author="Andrew Straw",
-    author_email="astraw@caltech.edu",
+    author_email="strawman@astraw.com",
     url="http://www.its.caltech.edu/~astraw/seppo.html",
-    package_dir = {'' : 'lib'},
-    py_modules = ['seppo'],
+    
+    package_dir = {'seppo' : 'lib/seppo'},
+    packages = ['seppo'],
+
+    entry_points = {'console_scripts': [
+    'seppo_serv_process = seppo:start_seppo_enslaved_server',
+    ],
+                    },
+    
     )
